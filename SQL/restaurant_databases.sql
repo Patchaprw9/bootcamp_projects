1e8632b1bfeed0f05ee85590ce4d8253a5773867
Patchaprw9 <126507519+Patchaprw9@users.noreply.github.com>
2023-02-28 16:23:58 +0700

    edit

--- SQL/restaurant_databases.sql
+++ SQL/restaurant_databases.sql
@@ -30,7 +30,7 @@ INSERT INTO dim_payment_type VALUES
   (2, 'Promtpay'),
    (3, 'Credit');
    
-CREATE TABLE employee (
+  CREATE TABLE employee (
   employeeId INT UNIQUE,
   name TEXT,
   position TEXT,
@@ -41,17 +41,29 @@ INSERT INTO employee VALUES
   (3, 'Pee', 'Staff'),
   (4, 'Toy', 'Chef');
   
+  
   CREATE TABLE customer (
   customerId INT UNIQUE,
   name TEXT,
   customer_gender TEXT,
-  PRIMARY KEY (customerId)
-);
-
+  PRIMARY KEY (customerId));
 INSERT INTO customer VALUES 
-  (1, 'Nack',  'M'),
-  (2, 'Whan',  'F'),
-  (3, 'Bow',  'F');
+  (1, 'Nack', 		'M'),
+  (2, 'Whan', 	   'F'),
+  (3, 'Bow', 		 'F'),
+  (4, 'Nun', 		 'F'),
+  (5, 'Boss', 			'M'),
+  (6, 'Fin', 		'M'),
+  (7, 'Dav',		'M'),
+  (8, 'Ekk', 		'M'),
+  (9, 'Burira',		'F'),
+  (10,'Kitty', 		'F'),
+  (11,'Emma',	'F'),
+  (12,'Will',		'F'),
+  (13,'Venus',  	'F'),
+  (14,'Phing',		'F'),
+  (15,'Tor', 		'M'),
+  (16,'Kik ',		'F');
   
   CREATE TABLE orders (
   ordersId INT UNIQUE,
@@ -67,40 +79,44 @@ INSERT INTO customer VALUES
 );
 
 INSERT INTO orders VALUES 
-  (1, 1, 2, '2022-02-09', 2500,1),
-  (2, 2, 2, '2022-02-28', 3560,2),
-  (3, 3, 4, '2022-02-09', 6540,1),
-  (4, 3, 4, '2022-02-07', 1750,1),
-  (5, 2, 1, '2022-03-18', 3650,1),
-  (6, 2, 2, '2022-03-28', 3560,2),
-  (7, 3, 4, '2022-03-10', 6540,3),
-  (8, 3, 4, '2022-03-15', 1750,2),
-  (9, 1, 2, '2022-03-04', 2500,2),
-  (10, 2, 2, '2022-03-29', 3560,2);
-
-SELECT * FROM menu;
+  (1,  1, 2, '2022-02-09', 250,1),
+  (2,  2, 2, '2022-02-28', 350,2),
+  (3,  3, 4, '2022-02-09', 640,1),
+  (4,  3, 4, '2022-02-07', 1750,1),
+  (5,  2, 1, '2022-03-18', 350,1),
+  (6,  2, 2, '2022-02-08', 260,2),
+  (7, 16 ,4,'2022-03-10', 640,3),
+  (8, 15, 4, '2022-03-15', 150,2),
+  (9, 14, 2,'2022-03-04', 2500,2),
+  (10, 12, 2, '2022-03-29', 360,2),
+  (11,13  ,2,'022-02-09', 1200,1),
+  (12, 2  , 2, '2022-02-08', 560,2),
+  (13, 3 , 4, '2022-02-09', 640,1),
+  (14, 6  ,4,'2022-03-04', 70,1),
+  (15, 8, 1, '2022-02-15', 350,1),
+  (16, 9, 2, '2022-02-15',3560,2),
+  (17,10, 4, '2022-02-15',640,3),
+  (18,4, 4, '2022-02-15',750,2),
+  (19,5, 2, '2022-03-04',2500,2),
+  (20, 6, 2, '2022-03-29',3560,2),
+  (21,7, 2, '2022-03-07',500,1),
+  (22,8, 2, '2022-03-08', 1360,2),
+  (23,9, 4, '2022-02-09', 640,1),
+  (24, 10,2,  '2022-03-07',1750,1),
+  (25,11, 1, '2022-03-18', 350,1),
+  (26, 12,1, '2022-03-18', 3550,1),
+  (27, 15,4,'2022-03-10', 540,3),
+  (28 ,13,4, '2022-03-15', 1750,2),
+  (29 ,12,2,'2022-02-04', 2500,2),
+  (30 , 8,2, '2022-02-29', 360,2),
+  (31 , 6,2, '2022-02-29', 380,2)
+  ;
+  
+ SELECT * FROM menu;
 SELECT * FROM customer;
 SELECT * FROM employee;
 SELECT * FROM dim_payment_type;
 SELECT * FROM orders;
-
-
-WITH totalSalesByCustomer AS (
-  SELECT
-  customer_gender,
-  orders.customerId,
-  ordersPrice
-FROM customer
-JOIN orders ON customer.customerId = orders.customerId
-)
-SELECT
-  customerId,
-  customer_gender,
-  SUM(ordersPrice) AS TotalSales
-FROM totalSalesByCustomer
-WHERE  customer_gender LIKE 'M'
-GROUP BY customerId
-ORDER BY totalSales DESC;
 
 SELECT
   Month,
@@ -116,3 +132,22 @@ FROM orders
 ) AS sub
 GROUP BY month
 ORDER BY Sum_orders_M DESC;
+
+WITH totalSalesByCustomer AS (
+  SELECT
+  customer_gender,
+  orders.customerId,
+  name,
+  ordersPrice
+FROM customer
+JOIN orders ON customer.customerId = orders.customerId
+)
+SELECT
+  customerId,
+  name,
+  customer_gender,
+  SUM(ordersPrice) AS TotalSales_Men
+FROM totalSalesByCustomer
+WHERE  customer_gender LIKE 'M'
+GROUP BY customerId
+ORDER BY TotalSales_Men DESC;
